513e77511f54ee0275315bad3a56b81a4ff858d6
Vijay Prasath PM <vpatturmohan@vmware.com>
2022-06-01 08:51:20 -0400

    Add leetcode tracker

---
+++ scripts/leetcode-tracker.js
@@ -0,0 +1,17 @@
+// This script is to keep track of problems which are already done or being re-done
+// I know "sessions" give you a cleaner way but if you for some reason can't use
+// it, this is the way to go. Just update "finished" and copy paste this in terminal of
+// the browser, you'll see all question which you finished striken off
+
+var finished = new Set([
+    1, 9,
+])
+var extractQNoFromNode = (node) => Number(node.innerText.split('. ')[0])
+var strikeThoughNode = (node) => node.style.setProperty("text-decoration", "line-through");
+var nodes = document.querySelectorAll('[role="table"] [role="row"] [role="cell"]:nth-child(2)')
+nodes.forEach((node) => {
+    var qNo = extractQNoFromNode(node);
+    if (finished.has(qNo)) {
+        strikeThoughNode(node)
+    }
+})
