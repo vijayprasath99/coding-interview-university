a982c7c8697cb307e393d3e2620e4b05a378ba22
Vijay Prasath PM <vpatturmohan@vmware.com>
2022-09-23 17:06:42 -0400

    Update leetcode-tracker.js

--- scripts/leetcode-tracker.js
+++ scripts/leetcode-tracker.js
@@ -1,17 +1,29 @@
-// This script is to keep track of problems which are already done or being re-done
-// I know "sessions" give you a cleaner way but if you for some reason can't use
-// it, this is the way to go. Just update "finished" and copy paste this in terminal of
-// the browser, you'll see all question which you finished striken off
+// ==UserScript==
+// @name         Leetcode question tracker
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  This script is to keep track of problems which are already done or being re-done
+// @author       You
+// @match        https://leetcode.com/problemset/all/**
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=leetcode.com
+// @grant        none
+// ==/UserScript==
 
-var finished = new Set([
-    1, 9, 13, 2
-])
-var extractQNoFromNode = (node) => Number(node.innerText.split('. ')[0])
-var strikeThoughNode = (node) => node.style.setProperty("text-decoration", "line-through");
-var nodes = document.querySelectorAll('[role="table"] [role="row"] [role="cell"]:nth-child(2)')
-nodes.forEach((node) => {
-    var qNo = extractQNoFromNode(node);
-    if (finished.has(qNo)) {
-        strikeThoughNode(node)
-    }
-})
+(function() {
+    'use strict';
+
+    var finished = new Set([
+        1, 9, 13, 2, 14, 509
+    ])
+    var extractQNoFromNode = (node) => Number(node.innerText.split('. ')[0])
+    var strikeThoughNode = (node) => node.style.setProperty("text-decoration", "line-through");
+    setInterval(() => {
+        var nodes = document.querySelectorAll('[role="table"] [role="row"] [role="cell"]:nth-child(2)')
+        nodes.forEach((node) => {
+            var qNo = extractQNoFromNode(node);
+            if (finished.has(qNo)) {
+                strikeThoughNode(node)
+            }
+        });
+    }, 3000);
+})();
